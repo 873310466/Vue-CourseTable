0e2a65b299a2ba1e8e169e946f8339234aaca629
lemonfishzc <5705784+lemonfishzc@user.noreply.gitee.com>
2020-04-13 13:50:41 +0800

    优化界面

--- src/views/CourseTable/CourseTable.vue
+++ src/views/CourseTable/CourseTable.vue
@@ -1,154 +1,161 @@
 <template>
-  <el-card shadow="hover" :style="cardBackground" >
-    <!--左上角-->
-    <div class="drop">
-      <!--Help-->
-      <el-dropdown class="helper" @command="handleCommand">
-        <el-button circle type="success" icon="el-icon-s-opportunity">
-        </el-button>
-        <el-dropdown-menu slot="dropdown">
-          <el-dropdown-item command="about">关于本站</el-dropdown-item>
-          <el-dropdown-item command="suggestion">友情建议</el-dropdown-item>
-        </el-dropdown-menu>
-      </el-dropdown>
-      <!--选择周数-->
-      <span class="select-week">
-      <common-form inline :form-label="formLabel" :form="formData">
-        <!--回到本周的按钮-->
-          <el-button-group>
-            <el-tooltip effect="light" content="显示上周课程" placement="top" :enterable="false">
-              <el-button :disabled="formData.week===1" @click="lastWeekCourses" type="primary"
-                         icon="el-icon-arrow-left"></el-button>
-            </el-tooltip>
-            <el-tooltip effect="light" content="显示本周课程" placement="top" :enterable="false">
-                          <el-button :disabled="formData.week===curWeek" class="btn-curweek" type='primary' round
-                                     icon="el-icon-location-information" @click="rebackCurWeek"></el-button>
-            </el-tooltip>
-            <el-tooltip effect="light" content="显示下周课程" placement="top" :enterable="false">
-            <el-button :disabled="formData.week===17" @click="nextWeekCourses" type="primary"
-                       icon="el-icon-arrow-right"></el-button>
-            </el-tooltip>
-          </el-button-group>
 
-      </common-form>
-    </span>
+      <el-card shadow="hover" :style="cardBackground" >
 
-    </div>
+        <el-row>
+            <!--左上角-->
+              <!--Help-->
+              <el-col :span="1">
+                <el-dropdown  @command="handleCommand">
+                  <el-button circle type="success" icon="el-icon-s-opportunity">
+                  </el-button>
+                  <el-dropdown-menu slot="dropdown">
+                    <el-dropdown-item command="about">关于本站</el-dropdown-item>
+                    <el-dropdown-item command="suggestion">友情建议</el-dropdown-item>
+                  </el-dropdown-menu>
+                </el-dropdown>
+              </el-col>
+              <el-col :xs="{span: 24, offset: 3}" :lg="{span: 10, offset: 6}">
+                <!--选择周数-->
+                <span class="select-week">
+          <common-form inline :form-label="formLabel" :form="formData">
+            <!--回到本周的按钮-->
+              <el-button-group>
+                <el-tooltip effect="light" content="显示上周课程" placement="top" :enterable="false">
+                  <el-button :disabled="formData.week===1" @click="lastWeekCourses" type="primary"
+                             icon="el-icon-arrow-left"></el-button>
+                </el-tooltip>
+                <el-tooltip effect="light" content="显示本周课程" placement="top" :enterable="false">
+                              <el-button :disabled="formData.week===curWeek" class="btn-curweek" type='primary' round
+                                         icon="el-icon-location-information" @click="rebackCurWeek"></el-button>
+                </el-tooltip>
+                <el-tooltip effect="light" content="显示下周课程" placement="top" :enterable="false">
+                <el-button :disabled="formData.week===17" @click="nextWeekCourses" type="primary"
+                           icon="el-icon-arrow-right"></el-button>
+                </el-tooltip>
+              </el-button-group>
+          </common-form>
+        </span>
+              </el-col>
+        </el-row>
 
-    <!--表格区域-->
-    <el-table
-      :header-cell-style="headerCellStyle"
-      :cell-style="cellStyle"
-      :data="tableData"
-      :span-method="objectSpanMethod"
-      v-loading="tableConfig.isLoading"
-      style="border-radius: 20px"
-      border>
-      <el-table-column
-        type="index"
-        align="center"
-        label="节次\星期"
-        width="95px"
-      >
-        <template slot-scope="scope">
-          <div class="first-col">
-            <div class="first-col-normal">
-              {{customIndex(scope.$index,'normal')}}
-            </div>
-            <div class="first-col-time">
-              {{customIndex(scope.$index,'time')}}
-            </div>
-          </div>
+        <!--表格区域-->
+        <el-table
+          fixed
+          :height="tableConfig.height"
+          :header-cell-style="headerCellStyle"
+          :cell-style="cellStyle"
+          :data="tableData"
+          :span-method="objectSpanMethod"
+          v-loading="tableConfig.isLoading"
+          style="border-radius: 20px"
+          border>
+          <el-table-column
+            type="index"
+            align="center"
+            label="节次\星期"
+            width="95px"
+          >
+            <template slot-scope="scope">
+              <div class="first-col">
+                <div class="first-col-normal">
+                  {{customIndex(scope.$index,'normal')}}
+                </div>
+                <div class="first-col-time">
+                  {{customIndex(scope.$index,'time')}}
+                </div>
+              </div>
 
-        </template>
-      </el-table-column>
-      <el-table-column
-        align="center"
-        v-for="item in tableLabel"
-        :key="item.prop"
-        :prop="item.prop"
-        :label="item.label"
-      >
-        <template slot-scope="scope">
-          <div class="course">
-            <div v-if="scope.row[item.prop].name!==undefined" slot="reference">
-              <el-tag size="medium">{{scope.row[item.prop].name}}</el-tag>
-            </div>
-            <div class="course-teacher">
-              {{scope.row[item.prop].teacher}}
-            </div>
-            <div class="course-location">
-              {{scope.row[item.prop].location}}
-            </div>
-          </div>
-        </template>
-      </el-table-column>
+            </template>
+          </el-table-column>
+          <el-table-column
+            align="center"
+            v-for="item in tableLabel"
+            :key="item.prop"
+            :prop="item.prop"
+            :label="item.label"
+          >
+            <template slot-scope="scope">
+              <div class="course">
+                <div v-if="scope.row[item.prop].name!==undefined" slot="reference">
+                  <el-tag size="medium">{{scope.row[item.prop].name}}</el-tag>
+                </div>
+                <div class="course-teacher">
+                  {{scope.row[item.prop].teacher}}
+                </div>
+                <div class="course-location">
+                  {{scope.row[item.prop].location}}
+                </div>
+              </div>
+            </template>
+          </el-table-column>
 
-    </el-table>
+        </el-table>
 
-    <!--加星星区域-->
-    <div class="stars">
-      <el-tooltip class="item" effect="light" content="加颗星星吧OwO~" placement="top-start">
-        <el-button @click="addStar" type="warning" icon="el-icon-star-on" circle/>
-      </el-tooltip>
-      <div class="starSum">{{starSum}}</div>
-    </div>
-    <!--About-->
-    <div class="about">
-      <el-drawer
-        title="关于本站"
-        :visible.sync="drawerVisible"
-        direction="ltr"
-      >
-        <ul class="about-ul">
-          <li>简介：本网站是自己上学的一个课程表~</li>
-          <li>作者：柠檬鱼</li>
-          <li>现状：东北大学-软件学院-大二学生</li>
-          <li>联系方式：873310466@qq.com</li>
-          <li>源码
-            <ul>
-              <li><span style="margin-right: 5px">前端 :</span>
-                <el-link type="primary" href="https://github.com/873310466/Vue-CourseTable">
-                  https://github.com/873310466/Vue-CourseTable
-                </el-link>
-              </li>
-              <li><span style="margin-right: 5px">后端 :</span>
-                <el-link type="primary" href="https://github.com/873310466/CoureseTable">
-                  https://github.com/873310466/CoureseTable
-                </el-link>
+        <!--加星星区域-->
+        <div class="stars">
+          <el-tooltip class="item" effect="light" content="加颗星星吧OwO~" placement="top-start">
+            <el-button @click="addStar" type="warning" icon="el-icon-star-on" circle/>
+          </el-tooltip>
+          <div class="starSum">{{starSum}}</div>
+        </div>
+        <!--抽屉页t-->
+        <div class="about">
+          <el-drawer
+            size="35%"
+            title="关于本站"
+            :visible.sync="drawerVisible"
+            direction="ltr"
+          >
+            <ul class="about-ul">
+              <li>简介：本网站是自己上学的一个课程表~</li>
+              <li>作者：柠檬鱼</li>
+              <li>现状：东北大学-软件学院-大二学生</li>
+              <li>联系方式：873310466@qq.com</li>
+              <li>源码
+                <ul>
+                  <li><span style="margin-right: 5px">前端 :</span>
+                    <el-link type="primary" href="https://github.com/873310466/Vue-CourseTable">
+                      https://github.com/873310466/Vue-CourseTable
+                    </el-link>
+                  </li>
+                  <li><span style="margin-right: 5px">后端 :</span>
+                    <el-link type="primary" href="https://github.com/873310466/CoureseTable">
+                      https://github.com/873310466/CoureseTable
+                    </el-link>
+                  </li>
+                </ul>
               </li>
             </ul>
-          </li>
-        </ul>
-        <img src="" alt="">
-        <el-carousel :interval="3000" arrow="always" style="margin: 0 13px;border-radius: 10px">
-          <el-carousel-item v-for="item in dataImg" :key="item.src">
-            <img style="width: 100%;height: 100%" :src="item.src"/>
-          </el-carousel-item>
-        </el-carousel>
-        <el-divider content-position="center">⭐ <span style="font-weight: bold;font-size: 16px">愿景</span> ⭐</el-divider>
-        <h4 style="display: flex;justify-content: center">希望自己早日找到实习，越来越 <span style="color: #00aae2"> 腻害</span>叭 (●'◡'●)</h4>
-      </el-drawer>
-    </div>
+            <img src="" alt="">
+            <el-carousel :interval="3000" arrow="always" style="margin: 0 13px;border-radius: 10px">
+              <el-carousel-item v-for="item in dataImg" :key="item.src">
+                <img style="width: 100%;height: 100%" :src="item.src"/>
+              </el-carousel-item>
+            </el-carousel>
+            <el-divider content-position="center">⭐ <span style="font-weight: bold;font-size: 16px">愿景</span> ⭐</el-divider>
+            <h4 style="display: flex;justify-content: center">希望自己早日找到实习，越来越 <span style="color: #00aae2"> 腻害</span>叭 (●'◡'●)</h4>
+          </el-drawer>
+        </div>
 
-    <!--友情建议对话框-->
-    <el-dialog
-      title="友情建议"
-      :visible.sync="leaveMessageDialogVisible"
-      ref="leaveMessageDialog"
-      :before-close="beforeCloseLMDialog"
-      width="50%"
-    >
-      <!--友情建议表单-->
-      <common-form :form-label="LMFormLabel" :form="LMFormData" :form-rules="LMFormRules"
-                   :label-position="'top'"></common-form>
+        <!--友情建议对话框-->
+        <el-dialog
+          title="友情建议"
+          :visible.sync="leaveMessageDialogVisible"
+          ref="leaveMessageDialog"
+          :before-close="beforeCloseLMDialog"
+          width="60%"
+        >
+          <!--友情建议表单-->
+          <common-form :form-label="LMFormLabel" :form="LMFormData" :form-rules="LMFormRules"
+                       :label-position="'top'"></common-form>
 
-      <span slot="footer" class="dialog-footer">
+          <span slot="footer" class="dialog-footer">
     <el-button @click="beforeCloseLMDialog">取 消</el-button>
     <el-button type="primary" @click="sendSuggestion">发 送</el-button>
   </span>
-    </el-dialog>
-  </el-card>
+        </el-dialog>
+      </el-card>
 
 </template>
 
@@ -291,11 +298,11 @@ export default {
       ],
       tableData: [],
       tableConfig: {
-        isLoading: true
+        isLoading: true,
+        height: window.innerHeight - 159
       }
     }
   },
-
   methods: {
     // 加星星
     async addStar () {
@@ -340,7 +347,10 @@ export default {
     },
     // 发送建议对话框BeforeClose
     beforeCloseLMDialog () {
-      this.$confirm('亲亲确认关闭对话框吗 OvO？')
+      this.$confirm('亲亲确认关闭对话框吗 OvO？', '提示', {
+        type: 'warning',
+        roundButton: true
+      })
         .then(_ => {
           this.leaveMessageDialogVisible = false
         })
@@ -520,23 +530,26 @@ export default {
         return row[column.property].rowSpan > 0
           ? {
             backgroundColor: '#E0FFFF'
-
           } : {}
       } else {
         if (rowIndex < 4) {
           return {
             backgroundColor: '#c4e1c5',
-            cursor: 'default'
+            cursor: 'default',
+            fontWeight: 'bold'
           }
         } else if (rowIndex < 8) {
           return {
             backgroundColor: 'skyblue',
-            cursor: 'default'
+            cursor: 'default',
+            fontWeight: 'bold'
+
           }
         } else {
           return {
             backgroundColor: '#FFB6C1',
-            cursor: 'default'
+            cursor: 'default',
+            fontWeight: 'bold'
           }
         }
       }
@@ -616,19 +629,18 @@ export default {
   }
 
   .el-card {
-    margin: 30px;
-    height: 780px;
     padding: 0 15px;
     background-size: cover!important;
-    .el-table {
+    /deep/ .el-table {
       opacity: 0.5;
       transition: 0.666s ease-in-out;
-
+      .el-table__body{
+        height: 100%;
+      }
     }
-  }
-
-  .el-table:hover {
-    opacity: 1;
+    .el-table:hover {
+      opacity: 1;
+    }
   }
 
   .select-week {
@@ -689,17 +701,13 @@ export default {
     opacity: 0;
     transition: 0.5s ease-in-out;
     position: absolute;
-    font-size: 14px;
+    font-size: 15px;
+    width: 70px;
+    color: steelblue;
     font-weight: bold;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%)
-  }
-
-  .helper {
-    position: absolute;
-    left: 50px;
-    top: 50px;
   }
 
   .about-ul li {
